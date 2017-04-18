2732938f82a45a624429da557d9902fab60d9d87
Thrainul <theSparhawk@gmail.com>
2017-04-18 12:31:39 +1200

    Implimeted the ErrorReporter Class
    Added functionality to the error reporter class, so that we can record
    errors and handle them

--- FortunaExcelProcessing/ErrorReporter.cs
+++ FortunaExcelProcessing/ErrorReporter.cs
@@ -3,28 +3,49 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using NPOI.SS.UserModel; 
 
 namespace FortunaExcelProcessing
 {
-    public class ErrorReporter
+    public static class ErrorReporter
     {
         private List <String> _errorList;
 
-        public ErrorReporter()
+        public static void HardErrorRecorder(string errorLine, ICell cell)
         {
-            //not sure if this is needed
-            _errorList = new List<string>();
+            _hardErrorList.Add(errorLine,cell);
         }
 
-        public void RecordErrors()
+        public static void RecordErrors()
         {
 
         }
 
-        public bool CheckStatus()
+        //THis method is supposed to check if any of the lists contain errors, and then
+        // return a specific number to indicate what list should be read.
+        public static int CheckStatus()
         {
-            return true;
+            if (_softErrorList.Count == 0 && _hardErrorList.Count == 0)
+            {
+                return 1;
+            }
+            else if (_softErrorList.Count == 0 && _hardErrorList.Count >0)
+            {
+                return 2;
+            }
+            else if (_hardErrorList.Count == 0 && _softErrorList.Count > 0)
+            {
+                return 3;
+            }
+            else
+                return 4;
+        }
+
+        //This method simply resets the errorLists
+        public static void ResetErrors()
+        {
+            _softErrorList = new List<string>();
+            _hardErrorList = new Dictionary<string, ICell>();
         }
     }
 }

b1bb006024029a9f94d5c355c1bfca8138340f8c
bhcode <brittney.huxford@gmail.com>
2017-09-10 11:54:19 +1200

    :sagittarius:

--- FortunaExcelProcessing/WeeklyProcessing/EditWeeklyDataTable.cs
+++ FortunaExcelProcessing/WeeklyProcessing/EditWeeklyDataTable.cs
@@ -13,15 +13,13 @@ namespace FortunaExcelProcessing.WeeklyProcessing
     {
         ISheet _sheet;
 
-<<<<<<< HEAD
         private int[] dataRows = { 3, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41 };
-=======
-        private int[] dataRows = {  3,5,6, 7, 8, 9, 10, 11, 12, 13, 14, 16, 17, 18, 19, 20, 21, 22,23, 24,25,26,27, 28,29,30,31, 32, 33, 34,35,36,37,38,39,40,41 };
->>>>>>> origin/master
+
         private string[] dataLabels = { "Week Ending:","Production","Honey (kg)", "Honey to Date (kg)" , "Avg Honey Per Hive (kg)", "Beeswax (kg)","Feeding","Honey Store","Pollen Store","Honey Feed","Pollen Feed","Ener-H-Plus","HFCS-55",
                                         "Vita Feed Gold","Pollen Patty","Living Conditions","Hive Condition","Temper","Odor","Population","Laying Pattern",
                                         "Area Information","Total Area (m^2)","Total Frames","Total Frames Unused","Death Information","Deaths","Deaths to Date",
                                         "Disease Information","Diseased Hives","Hives Treated","Replacement Hives","Bees Bought (kg)","Conditions","Avg Temperature","New Queens"};
+
         string sql; SQLiteCommand command; SQLiteConnection dbConn;
 
         public void EditTable(ISheet sheet)
@@ -58,7 +56,15 @@ namespace FortunaExcelProcessing.WeeklyProcessing
                         for (int row = 0; row < dataRows.Length - 1; row++)
                         {
                             if (row != dataRows.Length)
-                                output = output + CheckCellData.CellTypeNumeric(_sheet.GetRow(dataRows[row]).GetCell(c)) + ",";
+                            {
+                                if (_sheet.GetRow(dataRows[row]).GetCell(c) == null) {
+                                    continue;
+                                }
+                                else {
+                                    output = output + CheckCellData.CellTypeNumeric(_sheet.GetRow(dataRows[row]).GetCell(c)) + ",";
+                                }
+                            }
+                                
                         }
 
                         output = output + CheckCellData.CellTypeNumeric(_sheet.GetRow(dataRows.Length - 1).GetCell(c)) + "]";
